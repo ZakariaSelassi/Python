f3e69f86e3e30e6c5aae0d508429997791d1e276
zakaria <zakariaselassi@gmail.com>
2021-04-02 14:54:09 +0200

    update list exercice

---
+++ exercice3/exercice3.py
@@ -0,0 +1,20 @@
+import copy
+def insertList(list):    
+    listcpy = []
+    for x in range(len(list)):
+        if x < 5:
+        #Insert permet d'insert des valeurs dans un tableau à partir d'un index donnée 
+            listcpy.insert(x, list[x])
+    return print(listcpy)
+
+def addArray(list):     
+    num = int(input("entrer un nombre : "));
+    arr = []
+    for x in list:  
+        if x < num:
+            arr.append(x)
+    return print(arr)
+
+list = [1,2,3,4,5,6,7,8,9,10]
+insertList(list)
+addArray(list)
