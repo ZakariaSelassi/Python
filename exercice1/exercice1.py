f30eb0b9367b736f0388d745e9545227975f7b80
zakaria <zakariaselassi@gmail.com>
2021-04-02 11:01:44 +0200

    update commentaire ajouté

--- exercice1/exercice1.py
+++ exercice1/exercice1.py
@@ -1,6 +1,7 @@
 import datetime
 def ask():
     name = input('Give your name :')
+    # Attention si resulter entrer est un nombre le resultat obtenu sera un string !
     age = int(input('Give your age : '))
     now = datetime.datetime.now()
     calculate = str((now.year-age+100))
